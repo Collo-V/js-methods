bcab998ca8dcd6ce597373140b5abc29750e39c3
Collo-V <collinsvictoromondi@gmail.com>
2026-06-27 11:44:12 +0300

    add object functions clean,filter,and delete Obhject fields

--- src/methods/objects/plain-objects/index.ts
+++ src/methods/objects/plain-objects/index.ts
@@ -7,6 +7,7 @@ export function isPlainObject(obj:AnyObject) {
     return obj?.constructor === Object;
 }
 
+
 /* 
 *Trims whitespace from all string values in an object.
 */
@@ -16,4 +17,41 @@ export const trimObjectStrings = <T extends AnyObject>(object: T): T =>{
         acc[key as keyof T] = typeof value === "string" ? value.trim() : value;
         return acc;
       }, {} as T);
+};
+/* 
+*Trims whitespace from all string values in an object and removes undefined values.
+*/
+export const cleanObject = <T extends AnyObject>(object: T): T =>{
+    const cleaned: Partial<T> = {}
+    //@ts-expect-error ignore
+    Object.entries(object).forEach(([key, value]) => {
+        if(value === undefined)return;
+        if(typeof value === "string")value =  value.trim()
+        cleaned[key as keyof T] = value;
+      });
+    return cleaned as T;
+};
+
+/* 
+*Filters out fields not included in a given array of field names.
+*/
+export const filterObjectFields = <T extends AnyObject>(object: T, fields: (keyof T)[]): T =>{
+    //@ts-expect-error ignore
+    return Object.entries(object).reduce((acc, [key, value]) => {
+        if (!fields.includes(key)) return acc;
+        acc[key as keyof T] = value;
+        return acc;
+      }, {} as T);
+};
+/* 
+*Deletes fields in the given array from an object.
+*/
+export const deleteObjectFields = <T extends AnyObject>(object: T, fields: (keyof T)[]): T =>{
+    //@ts-expect-error ignore
+    return Object.entries(object).reduce((acc, [key, value]) => {
+        if (!fields.includes(key)) {
+            acc[key as keyof T] = value;
+        }
+        return acc;
+      }, {} as T)
 };
